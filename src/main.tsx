fd6d94d15ba86a9d821a8b14961baf1dd2be63ee
Your Name <you@example.com>
2024-11-29 15:22:06 +0400

    fix build

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { ReactQueryDevtools } from 'react-query-devtools';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
 import 'normalize.css';
 
@@ -11,7 +9,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
       <BrowserRouter>
         <App />
-        <ReactQueryDevtools initialIsOpen={false} /> {/* Or set to true to open by default */}
       </BrowserRouter>
   </React.StrictMode>
 )
